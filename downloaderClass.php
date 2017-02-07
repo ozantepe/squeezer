724d43510d3369ead8d2214b55d6589277023635
SYMUJDECI <l1114016@std.yildiz.edu.tr>
2017-02-07 20:10:51 +0300

    downloaderclass done 0

--- downloaderClass.php
+++ downloaderClass.php
@@ -1,31 +1,88 @@
 <?php
 
-class downloader () {
+class Downloader {
 	
-	private downloadList;
-	private dataList;
-	private zippedData;
+	private $downloadList = array();
+	private $dataList = array();
+	private $fileCounter = 0;
+	private $dirPath;
+	private $zippedDataPath;
 	
-	function downloader($downloadList) {
+	public function __construct($downloadList) {
+		$this->downloadList = $downloadList;
+	}
+	
+	public function prepareDownloading() {
+		$date = new DateTime();
+		$this->dirPath = "docs/".(string)$date->getTimestamp();
+		mkdir($this->dirPath, 0777, true); // mode will change for security
+	}
+	
+	public function multiDownload() {
+		foreach ($this->downloadList as $dLink) {
+			$this->download($dLink, false);
+		}
+	}
+	
+	public function download($link) {
+		$file = file_get_contents($link); // file has token to main memory
+		if (!$file) return;	// problem controller
+		
+		// extension founder and path creator
+		$pieces = explode('.',$link);
+		$fileName = (string)$this->fileCounter.'.'.end($pieces);
+		$this->fileCounter++;
+		$filePath = $this->dirPath.'/'.$fileName;
+		
+		// file creation
+		file_put_contents($filePath, $file);
+		$this->dataList[$fileName] = $filePath;
+		
+		// can be added immediate download for single document without saving data
 		
 	}
 	
-	function prepareDownloading() {
+	public function zipData() {
+		// zip file creation
+		$zip = new ZipArchive();
+		$this->zippedDataPath = $this->dirPath."/squeezer.zip";
+		if ($zip->open($this->zippedDataPath, ZIPARCHIVE::CREATE) !== true) {
+			echo 'problem<br>';
+			return;
+		}
 		
+		// adding files
+		foreach(array_keys($this->dataList) as $key) {
+			$zip->addFile($this->dataList[$key], $key);
+		}
+		$zip->close();
 	}
 	
-	function download() {
-		
+	public function getZippedData () {
+		return $this->zippedDataPath;
 	}
 	
-	function zipData() {
-		
-	}
-	
-	function getZippedData () {
+	function __destruct() {
 		
 	}
 	
 }
 
+// test area
+$links = array(
+"https://www.ce.yildiz.edu.tr/user/photo/thumb/131/hhb.png",
+"https://www.ce.yildiz.edu.tr/user/photo/thumb/24/NA.jpg",
+"https://www.ce.yildiz.edu.tr/user/photo/thumb/30/agy.png",
+"https://www.ce.yildiz.edu.tr/user/photo/thumb/61/fk.png",
+"https://www.ce.yildiz.edu.tr/user/photo/thumb/29/mek.jpg"
+);
+
+$d = new Downloader($links);
+$d->prepareDownloading();
+$d->multiDownload();
+$d->zipData();
+echo $d->getZippedData();
+
+
 ?>
+
