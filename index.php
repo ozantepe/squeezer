17188a734c251c6eb62ddecf38c4473cf81436f7
Ozan Tepe <ozantepe332@gmail.com>
2017-02-15 22:32:16 +0300

    web design

--- index.php
+++ index.php
@@ -1,47 +1,24 @@
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Squeezer</title>
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
-	</head>
-	<body>
-		<div class="ui vertical compact menu">
-			<div class="item">
-				<a href="#">Home</a>
-			</div>
-			<div class="item">
-				<a href="#">About</a>
-			</div>
-			<div class="item">
-				<a href="#">Contact</a>
-			</div>
-		</div>
-		<div class="ui container">
-			<h1 class="ui header">Welcome to Squeezer</h1>
-			<form class="ui form" action="" method="post">
-				<label for="seed_page">Please enter the url to search</label>
-				<div class="ten wide field">
-					<input type="text" name="seed_page" placeholder="Search...">
-				</div>
-				<label for="data_type">Please select the extension</label>
-				<div class="two wide field">
-					<select class="ui compact selection dropdown" name="data_type">
-						<option value="document">Document</option>
-						<option value="image">Image</option>
-						<option value="video">Video</option>
-						<option value="audio">Audio</option>
-						<option value="script">Script</option>
-					</select>
-					<br><br>
-					<button type="submit" name="submit" class="ui button">Search</button>	
-				</div>
-			</form>	
-		</div>
-	</body>
-</html>
-
 <?php
-	if(isset($_POST['submit'])) {
+	// Define variables and set to empty values
+	$seedPage = "";
+	$seedPageErr = "";
+	$validation = true;
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  if (empty($_POST["seedPage"])) {
+	    $seedPageErr = "*Please enter a url to search";
+	    $validation = false;
+	  } else {
+	    $seedPage = test_input($_POST["seedPage"]);
+	    // Check if URL address syntax is valid 
+	    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$seedPage)) {
+	      $seedPageErr = "Invalid URL"; 
+	      $validation = false;
+	    }
+	  } 
+	}
+
+	if(isset($_POST['submit']) && $validation) {
 		// Start session if submitted
 		session_start();
 
@@ -49,7 +26,7 @@
 		$data_types = array('document', 'image', 'video', 'audio', 'script');
 		$selected_data_type = htmlspecialchars($_POST['data_type']);
 		$depth = 1;
-		$seedPage = htmlspecialchars($_POST['seed_page']);
+		$seedPage = htmlspecialchars($_POST['seedPage']);
 
 		// Create crawler
 		include 'crawlerClass.php';
@@ -59,7 +36,75 @@
 		$downloadList = $crawler->getDownloadList();
 
 		// Transfer download list to result page
-		$_SESSION['downloadList'] = $downloadList;
-		header('Location: result.php');				
+		$token = md5(uniqid());
+		$_SESSION[$token] = $downloadList;
+		echo '
+		<form id= "token_form" action="result.php" method="post">
+		<input type="hidden" name="token_id" value="'.$token.'">
+		</form>
+		<script>
+			document.getElementById("token_form").submit();
+		</script>
+		';
+	}
+
+	function test_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
 	}
 ?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Squeezer</title>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+	</head>
+	<body>
+		<div class="ui container">
+			<div class="ui horizontal menu">
+				<div class="item">
+					<a href="index.php">Home</a>
+				</div>
+				<div class="item">
+					<a href="about.html">About</a>
+				</div>
+				<div class="item">
+					<a href="contact.html">Contact Us</a>
+				</div>
+			</div>
+			<h1 class="ui center aligned grey header">Welcome to SQUEEZER</h1>
+			<form class="ui form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+				<div class="ui segments">
+					<div class="ui yellow inverted segment">
+						<div class="ui grey label" for="seedPage">
+							Please enter the url to search
+						</div>
+						<div class="field" name="URL">
+							<input type="text" name="seedPage" placeholder="Search..." value="<?php echo $seedPage;?>">
+							<span class="error"><?php echo $seedPageErr;?></span>
+						</div>
+					</div>
+					<div class="ui yellow inverted segment">
+						<div class="ui grey label" for="data_type">
+							Please select the data type
+						</div>
+						<div class="two wide field" name="data_type">
+							<select class="ui simple dropdown" name="data_type">
+								<option value="document">Document</option>
+								<option value="image">Image</option>
+								<option value="video">Video</option>
+								<option value="audio">Audio</option>
+								<option value="script">Script</option>
+							</select>
+						</div>
+					</div>
+					<div class="ui yellow inverted segment">
+						<button type="submit" name="submit" class="ui grey button">Search</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
--- result.php
+++ result.php
@@ -1,55 +1,122 @@
 <?php
 	// Start session to retrieve values from server session
-	session_start(); 
-	$downloadList = $_SESSION['downloadList'];
+	session_start();
+	if (isset($_POST["token_id"]) && isset($_SESSION[$_POST["token_id"]])) {
+		$downloadList = $_SESSION[$_POST["token_id"]];
+		unset($_SESSION[$_POST["token_id"]]);
+	}
 ?>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Squeezer</title>
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	</head>
 	<body>
-		<div class="ui vertical compact menu">
-			<div class="item">
-				<a href="#">Home</a>
-			</div>
-			<div class="item">
-				<a href="#">About</a>
-			</div>
-			<div class="item">
-				<a href="#">Contact</a>
-			</div>
-		</div>
+		<?php
+			// If links selected and the download button clicked,
+			if(isset($_POST['submit']) && isset($_POST['link'])) {
+				// Add selected links to download list
+				$downloadList = $_POST['link'];
+
+				// Create downloader and download the list
+				include 'downloaderClass.php';
+				$downloader = new Downloader($downloadList);
+				$downloader->prepareDownloading();
+				$downloader->multiDownload();
+				$downloader->zipData();
+				$downloader->downloadZippedData();
+				exit();
+			}else {
+		?>
 		<div class="ui container">
-			<h1 class="ui header">Thanks for using Squeezer...</h1>
+			<div class="ui horizontal menu">
+				<div class="item">
+					<a href="index.php">Home</a>
+				</div>
+				<div class="item">
+					<a href="about.html">About</a>
+				</div>
+				<div class="item">
+					<a href="contact.html">Contact Us</a>
+				</div>
+			</div>
+			<h1 class="ui center aligned grey header">Thanks for using SQUEEZER...</h1>
 			<form class="ui form" action="" method="post">
-				<label>Here are the squeezed links:</label><br><br>
-				<?php
-					foreach ($downloadList as $link) {
-						echo '<input type="checkbox" name="link[]" value="'.$link.'"> '.$link.'<br>';
-					}
-				?>
-				<br>
-  				<label>Select your squeezed items then hit the download button !</label><br><br>
-				<button type="submit" name="submit" class="ui button">Download</button><br><br>
-			</form>	
+				<div class = "ui segments">
+					<div class="ui yellow inverted segment">
+						<div class="ui grey label">
+							<label>Here are the squeezed links</label>
+						</div>
+					</div>
+					<div class="ui yellow inverted segment">	
+						<div class="ui container">
+							<div class="ui grey inverted segment">
+								<?php
+									if (isset($downloadList)) {
+										foreach ($downloadList as $link) {
+											echo '
+											<div class="field">
+												<div class="ui checkbox">
+													<input class="checkbox" type="checkbox" name="link[]" value="'.$link.'">
+													<label>'.$link.'</label>
+												</div>
+											</div>
+											';
+										}
+									}else {
+										header("Location: index.php");
+									}
+								?>
+								<br>
+								<div class="field">
+									<div class="ui checkbox">
+										<input class="checkbox" type="checkbox" name="select_all" id="select_all">
+										<label>Select All</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class = "ui yellow inverted segment">
+						<div class ="grouped fields">
+							<div class="field">
+								<div class="ui bottom attached grey label">
+									<label>Select your squeezed items then hit the download button !</label>
+								</div>
+							</div>
+							<div class="field">
+								<button type="submit" name="submit" class="ui grey button">Download</button>
+							</div><br>
+						</div>
+					</div>
+				</div>
+			</form>
 		</div>
-	</body>
+		<?php
+			}
+		?>
+		<script type="text/javascript">	
+			// Select all checkboxes
+			$("#select_all").change(function() {  // "select_all" change
+				// Change all ".checkbox" checked status 
+			    $(".checkbox").prop('checked', $(this).prop("checked"));
+			});
+
+			// ".checkbox" change 
+			$('.checkbox').change(function() { 
+			    // Uncheck "select_all", if one of the listed checkbox item is unchecked
+			    if(false == $(this).prop("checked")){ // If this item is unchecked
+			    	// Change "select_all" checked status to false
+			        $("#select_all").prop('checked', false); 
+			    }
+			    // Check "select_all" if all checkbox items are checked
+			    if ($('.checkbox:checked').length == $('.checkbox').length ){
+			        $("#select_all").prop('checked', true);
+			    }
+			});
+		</script>
+		</body>
 </html>
 
-<?php
-	// If links selected and the download button clicked,
-	if(isset($_POST['submit']) && isset($_POST['link'])) {
-		// Add selected links to download list
-		$downloadList = $_POST['link'];
-
-		// Create downloader and download the list
-		include 'downloaderClass.php';
-		$downloader = new Downloader($downloadList);
-		$downloader->prepareDownloading();
-		$downloader->multiDownload();
-		$downloader->zipData();
-		$downloader->downloadZippedData();
-	}
-?>
